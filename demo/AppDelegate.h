d72cc268e60d471fb03424cd6b022d5a5bcf75e3
hellertang <hellertang@tencent.com>
2019-01-02 14:33:24 +0800

    Initial Commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by hellertang on 2019/1/2.
+//  Copyright © 2019年 hellertang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
